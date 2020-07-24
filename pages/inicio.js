e82de9bcfa3e9af68cb7eba63b4c9b3a65e67014
Luis Chilo <gfxargentina@gmail.com>
2020-07-24 19:42:14 -0300

    nav links add

--- pages/inicio.js
+++ pages/inicio.js
@@ -18,6 +18,12 @@ const Inicio = () => {
                      <svg id="Capa_1" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWwidth="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full" ><g><path d="m512 482h-30v-302h-91v302h-30v-182h-90v182h-30v-242h-90v242h-30v-152h-91v152h-30v30h512z"/><path d="m512 120v-120h-121v30h69.789l-144.789 143.789-120-120-191.605 190.606 21.21 21.21 170.395-169.394 120 120 166-165v68.789z"/></g></svg>
                        <span className="ml-3 text-xl">LUIS CRM</span>
                      </a>
+                     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+                            <a class="mr-5 hover:text-gray-900">Portfolio</a>
+                            <a class="mr-5 hover:text-gray-900">Nosotros</a>
+                            <a class="mr-5 hover:text-gray-900">Contacto</a>
+                            <a class="mr-5 hover:text-gray-900">RH</a>
+                          </nav>
                      
                      <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 mr-2 md:mt-0">Entrar
                        <svg fill="white" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
