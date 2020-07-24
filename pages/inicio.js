c5a3d9ab24429090b29d0daba280008d27d0483d
Luis Chilo <gfxargentina@gmail.com>
2020-07-24 20:13:46 -0300

    fix problem with a closure tag

--- pages/inicio.js
+++ pages/inicio.js
@@ -38,8 +38,8 @@ const Inicio = () => {
                      </button>
                    </div>
                  </header>
-
-                 <section className="text-gray-700 body-font mt-5">
+            </div>
+            <div className="text-gray-700 body-font mt-5">
                            <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
                              <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">CRM para manejo
@@ -54,12 +54,12 @@ const Inicio = () => {
                                </div>
                              </div>
                              <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-                               <img className="object-cover object-center rounded" alt="hero" src="https://i.ibb.co/FJgDjKK/CRM-1.jpg">
+                               <img className="object-cover object-center rounded" alt="hero" src="https://i.ibb.co/FJgDjKK/CRM-1.jpg"> </img>
                              </div>
                            </div>
-                         </section>
-            </div>
-            </>
+                         </div>
+
+       </>
      );
 }
  
