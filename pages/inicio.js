bd2fc253ce7fe4a7e317a3f12c2c3705ad5186a3
Luis Chilo <gfxargentina@gmail.com>
2020-07-24 19:40:15 -0300

    fix svg color

--- pages/inicio.js
+++ pages/inicio.js
@@ -20,7 +20,7 @@ const Inicio = () => {
                      </a>
                      
                      <button className="inline-flex items-center bg-gray-200 border-0 py-1 px-3 focus:outline-none hover:bg-gray-300 rounded text-base mt-4 mr-2 md:mt-0">Entrar
-                       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                       <svg fill="white" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
                          <path d="M5 12h14M12 5l7 7-7 7"></path>
                        </svg>
                      </button>
