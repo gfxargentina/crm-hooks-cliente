e7d52f943efd80199b9b15a3689296681d77e12e
Luis Chilo <gfxargentina@gmail.com>
2020-07-25 15:16:51 -0300

    fix margin of content in inicio

--- pages/inicio.js
+++ pages/inicio.js
@@ -53,7 +53,7 @@ const Inicio = () => {
                  </header>
             </div>
             <div className="text-gray-700 body-font mt-5 mb-5 flex-grow">
-                           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                           <div className="container mx-auto flex px-5 py-24 mb-5 md:flex-row flex-col items-center">
                              <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">CRM para manejo
                                   de Clientes, Productos y Pedidos.
